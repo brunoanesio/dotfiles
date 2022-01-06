946783965a6a5c93e9083c566eaae8fdf1c7936a
Bruno Anesio <brunohanesio@gmail.com>
2022-01-06 04:22:39 -0300

    Fixed path in symlinks

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # create symlinks
-ln -s .aliases $HOME/.aliases
-ln -s .zshrc $HOME/.zshrc
-ln -s .gitconfig $HOME/.gitconfig
+ln -s $HOME/.dotfiles/.aliases $HOME/.aliases
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
