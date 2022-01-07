f34299e990d751c437970c764028bdfd1469c33c
Bruno Anesio <brunohanesio@gmail.com>
2022-01-07 04:11:09 -0300

    Added MangoHud and paru conf files

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@
 ln -s $HOME/.dotfiles/.aliases $HOME/.aliases
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 ln -s $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
+ln -s ~/.dotfiles/.config/MangoHud ~/.config/MangoHud
+ln -s ~/.dotfiles/.config/paru ~/.config/paru
