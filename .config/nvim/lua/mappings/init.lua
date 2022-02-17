1879f9e7af45f680c152ff2887dc64c38c331deb
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-02-16 23:24:37 -0300

    Update nvim config

--- .config/nvim/lua/mappings/init.lua
+++ .config/nvim/lua/mappings/init.lua
@@ -31,4 +31,5 @@ map('n', '<leader>ff', ':Telescope find_files<CR>', opts)
 map('n', '<leader>fg', ':Telescope live_grep<CR>', opts)
 map('n', '<leader>fb', ':Telescope buffers<CR>', opts)
 map('n', '<leader>fh', ':Telescope help_tags<CR>', opts)
-
+-- change splits
+map('n', '<A-a>', '<C-W>w', opts)
