bb53440bd61cd30d1d87814e91b709bad7c8807e
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-02-18 18:53:58 -0300

    Update nvim config

--- .config/nvim/lua/plugins/nvimtree.lua
+++ .config/nvim/lua/plugins/nvimtree.lua
@@ -1,8 +1,8 @@
 require("nvim-tree").setup({
 	auto_close = true,
 	update_cwd = true,
-	open_on_setup = true,
-	open_on_tab = true,
+	open_on_setup = false,
+	open_on_tab = false,
 	diagnostics = {
 		enable = true,
 	},
