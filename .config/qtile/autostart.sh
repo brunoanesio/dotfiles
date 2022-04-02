6a799ccbcff538c2d6a5a1510c026444ea194430
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-04-02 03:22:07 -0300

    Update qtile autostart

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 xrandr --output HDMI-A-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal &
 nitrogen --restore &
 setxkbmap -layout br &
