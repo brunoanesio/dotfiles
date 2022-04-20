98f9b7c675b2488fea559cbb62fc658dc6d1a62e
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-04-20 04:28:11 -0300

    Fixed bufferline config

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,7 +1,8 @@
 require("bufferline").setup({
-	diagnostics = "nvim_lsp",
-	diagnostics_update_insert = true,
-    buffer_close_icon = '',
-    close_icon = '',
-    separator_style = "slant"
+    options = {
+        diagnostics = "nvim_lsp",
+        diagnostics_update_insert = true,
+        buffer_close_icon = '',
+        close_icon = '',
+    }
 })
