2b8d528b5a5e8ff06344ba8c9f468009b5476738
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-05-03 22:42:22 -0300

    Update lsp config

--- .config/nvim/lua/lsp-config/language-servers.lua
+++ .config/nvim/lua/lsp-config/language-servers.lua
@@ -1,98 +1,112 @@
-local lsp_installer = require("nvim-lsp-installer")
+local servers = { "pyright", "sumneko_lua", "html", "eslint", "cssls" }
+require("nvim-lsp-installer").setup({
+	ensure_installed = servers,
+	automatic_installation = true,
+	ui = {
+		icons = {
+			server_installed = "✓",
+			server_pending = "➜",
+			server_uninstalled = "✗",
+		},
+	},
+})
 
-local servers = {
-    "sumneko_lua",
-    "cssls",
-    "eslint",
-    "pyright",
-    "html",
-    "bashls",
-}
+vim.o.updatetime = 250
+local lspconfig = require("lspconfig")
+local function on_attach(_, bufnr)
+	vim.api.nvim_create_autocmd("CursorHold", {
+		buffer = bufnr,
+		callback = function()
+			local opts = {
+				focusable = false,
+				close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+				border = "rounded",
+				source = "always",
+				prefix = " ",
+				scope = "cursor",
+			}
+			vim.diagnostic.open_float(nil, opts)
+		end,
+	})
+	-- Set up buffer-local keymaps (vim.api.nvim_buf_set_keymap()), etc.
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
 
-local function on_attach(client, bufnr)
-    -- Set up buffer-local keymaps (vim.api.nvim_buf_set_keymap()), etc.
-    local function buf_set_keymap(...)
-        vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
+	local function buf_set_option(...)
+		vim.api.nvim_buf_set_option(bufnr, ...)
+	end
 
-    local function buf_set_option(...)
-        vim.api.nvim_buf_set_option(bufnr, ...)
-    end
+	-- Enable completion triggered by <c-x><c-o>
+	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
-    -- Enable completion triggered by <c-x><c-o>
-    buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+	-- Mappings.
+	local opts = { noremap = true, silent = true }
 
-    -- Mappings.
-    local opts = { noremap = true, silent = true }
-
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    buf_set_keymap("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-    buf_set_keymap("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-    buf_set_keymap("n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-    buf_set_keymap("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-    buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    buf_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-    buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    buf_set_keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format { async=true }<CR>", opts)
-    -- buf_set_keymap("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-    buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-    buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-    buf_set_keymap("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-
-    if client.name == "tsserver" then
-        client.server_capabilities.documentFormattingProvider = false
-    end
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+	buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+	buf_set_keymap("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+	buf_set_keymap("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+	buf_set_keymap("n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+	buf_set_keymap("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+	buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	buf_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+	buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+	buf_set_keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format { async=true }<CR>", opts)
+	-- buf_set_keymap("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+	buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+	buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+	buf_set_keymap("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
 end
 
-local enhance_server_opts = {
-    ["html"] = function(opts)
-        opts.filetypes = {
-            "html",
-            "htmldjango",
-        }
-        opts.init_options = {
-            embeddedLanguages = {
-                css = true,
-                javascript = true,
-            },
-            provideFormatter = false,
-        }
-    end,
-    ["pylsp"] = function(opts)
-        opts.plugins = {
-            flake8 = { enabled = true },
-        }
-    end,
-}
+-- nvim-cmp supports additional completion capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
--- Add additional capabilities supported by nvim-cmp
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
-
-lsp_installer.on_server_ready(function(server)
-    local opts = {
-        capabilities = capabilities,
-        on_attach = on_attach,
-    }
-
-    if enhance_server_opts[server.name] then
-        -- Enhance the default opts with the server-specific ones
-        enhance_server_opts[server.name](opts)
-    end
-
-    server:setup(opts)
-end)
-
-for _, name in pairs(servers) do
-    local server_is_found, server = lsp_installer.get_server(name)
-    if server_is_found then
-        if not server:is_installed() then
-            print("Installing " .. name)
-            server:install()
-        end
-    end
+-- Enable the following language servers
+for _, servers in ipairs(servers) do
+	lspconfig[servers].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
+
+-- LSP languages config
+lspconfig.html.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	filetypes = { "html", "htmldjango" },
+	init_options = {
+		embeddedLanguages = {
+			configurationSection = { "html", "css", "javascript" },
+			css = true,
+			javascript = true,
+		},
+		provideFormatter = false,
+	},
+})
+lspconfig.bashls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+-- require("lspconfig").pylsp.setup({
+-- 	on_attach = on_attach,
+-- 	capabilities = capabilities,
+-- 	settings = {
+-- 		pylsp = {
+-- 			plugins = {
+-- 				flake8 = { enabled = true },
+-- 			},
+-- 		},
+-- 	},
+-- })
+
+-- LSP UI config
+vim.diagnostic.config({
+	update_in_insert = true,
+})
--- .config/nvim/lua/lsp-config/null-ls-config.lua
+++ .config/nvim/lua/lsp-config/null-ls-config.lua
@@ -4,31 +4,44 @@ local formatting = null_ls.builtins.formatting
 local code_actions = null_ls.builtins.code_actions
 local diagnostics = null_ls.builtins.diagnostics
 
-local sources = {
-	-- python and django
-	formatting.black,
-	diagnostics.flake8,
-	formatting.djlint,
-	diagnostics.djlint,
-	-- lua
-	formatting.stylua,
-	-- js,ts,etc
-	formatting.prettier,
-	code_actions.eslint,
-	-- bash
-	formatting.shfmt,
-}
+local lsp_formatting = function(bufnr)
+    vim.lsp.buf.format({
+        filter = function(clients)
+            return vim.tbl_filter(function(client)
+                return client.name ~= "sumneko_lua"
+            end, clients)
+        end,
+        bufnr = bufnr,
+    })
+end
 
-null_ls.setup({
-	sources = sources,
-	on_attach = function(client)
-		if client.server_capabilities.documentFormattingProvider then
-			vim.cmd([[
-            augroup LspFormatting
-                autocmd! * <buffer>
-                autocmd BufWritePre <buffer> lua vim.lsp.buf.format()
-            augroup END
-            ]])
-		end
-	end,
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+require("null-ls").setup({
+    sources = {
+        -- python and django
+        formatting.black,
+        diagnostics.flake8,
+        formatting.djlint,
+        diagnostics.djlint,
+        -- lua
+        formatting.stylua,
+        -- js,ts,etc
+        formatting.prettier,
+        code_actions.eslint,
+        -- bash
+        formatting.shfmt,
+    },
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    lsp_formatting(bufnr)
+                end,
+            })
+        end
+    end,
 })
