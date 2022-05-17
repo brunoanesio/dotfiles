bd02c196f1b567644af7f319fd53952ca2ef34bf
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-05-16 21:17:11 -0300

    Update bufferline config

--- config/.config/nvim/lua/plugins/bufferline.lua
+++ config/.config/nvim/lua/plugins/bufferline.lua
@@ -4,5 +4,6 @@ require("bufferline").setup({
 		diagnostics_update_insert = true,
 		offsets = { { filetype = "NvimTree", text = "File Explorer" } },
 		always_show_bufferline = false,
+		show_close_icon = false,
 	},
 })
