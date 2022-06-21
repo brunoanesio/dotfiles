b6090988ea2bce7dca2fb6b863d537adeb4721fe
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-06-21 16:32:22 -0300

    Update whichkey.lua

--- config/.config/nvim/lua/plugins/whichkey.lua
+++ config/.config/nvim/lua/plugins/whichkey.lua
@@ -168,6 +168,7 @@ local mappings = {
 		p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
 		g = { "<cmd>lua _LAZYGIT_TOGGLE()<cr>", "Lazygit" },
 		f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
+		t = { "<cmd>ToggleTerm direction=tab<cr>", "Tab" },
 		h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
 		v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
 	},
