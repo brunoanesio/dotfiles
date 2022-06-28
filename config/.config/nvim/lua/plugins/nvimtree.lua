66165ae5f829e755264eb2558f1acc77106e19cb
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-06-28 04:22:18 -0300

    Update nvim tree config

--- config/.config/nvim/lua/plugins/nvimtree.lua
+++ config/.config/nvim/lua/plugins/nvimtree.lua
@@ -2,7 +2,26 @@ require("nvim-tree").setup({
 	update_cwd = true,
 	open_on_setup = false,
 	open_on_tab = false,
+	sync_root_with_cwd = true,
+	renderer = {
+		highlight_git = true,
+		indent_markers = {
+			enable = true,
+			icons = {
+				corner = "└ ",
+				edge = "│ ",
+				item = "│ ",
+				none = "  ",
+			},
+		},
+	},
 	diagnostics = {
 		enable = true,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
 	},
 })
