771b3c347da4925afdd84e514e6ee6cde49abea7
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-07-04 10:50:05 -0300

    Remove unused telescope extension

--- config/.config/nvim/lua/user/plugins/telescope.lua
+++ config/.config/nvim/lua/user/plugins/telescope.lua
@@ -11,16 +11,9 @@ require("telescope").setup({
 			override_file_sorter = true,
 			case_mode = "smart_case",
 		},
-		media_files = {
-			-- filetypes whitelist
-			-- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-			filetypes = { "png", "webp", "jpg", "jpeg" },
-			find_cmd = "rg", -- find command (defaults to `fd`)
-		},
 	},
 })
 require("telescope").load_extension("fzf")
-require("telescope").load_extension("media_files")
 require("telescope").load_extension("neoclip")
 require("telescope").load_extension("file_browser")
 require("telescope").load_extension("project")
