2da8aa4a8fa26ef95c49c5d732a7ade567700e59
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-07-04 10:50:12 -0300

    Update mappings

--- config/.config/nvim/lua/user/mappings/init.lua
+++ config/.config/nvim/lua/user/mappings/init.lua
@@ -1,7 +1,7 @@
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
-map("", "<Space>", "<Nop>", opts)
+-- map("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
