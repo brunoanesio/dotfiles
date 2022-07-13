734004c5390c395483dbd06da0b756668e34614d
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-07-13 12:46:47 -0300

    Update surround config

--- config/.config/nvim/lua/user/plugins/surround.lua
+++ config/.config/nvim/lua/user/plugins/surround.lua
@@ -1,6 +1,7 @@
 require("nvim-surround").setup({
 	keymaps = { -- vim-surround style keymaps
 		insert = "ys",
+		insert_line = "yss",
 		visual = "S",
 		delete = "ds",
 		change = "cs",
@@ -15,6 +16,19 @@ require("nvim-surround").setup({
 			[">"] = { "<", ">" },
 			["["] = { "[ ", " ]" },
 			["]"] = { "[", "]" },
+			-- Define pairs based on function evaluations!
+			["i"] = function()
+				return {
+					require("nvim-surround.utils").get_input("Enter the left delimiter: "),
+					require("nvim-surround.utils").get_input("Enter the right delimiter: "),
+				}
+			end,
+			["f"] = function()
+				return {
+					require("nvim-surround.utils").get_input("Enter the function name: ") .. "(",
+					")",
+				}
+			end,
 		},
 		separators = {
 			["'"] = { "'", "'" },
@@ -22,14 +36,20 @@ require("nvim-surround").setup({
 			["`"] = { "`", "`" },
 		},
 		HTML = {
-			["t"] = true, -- Use "t" for HTML-style mappings
+			["t"] = "type", -- Change just the tag type
+			["T"] = "whole", -- Change the whole tag contents
 		},
 		aliases = {
 			["a"] = ">", -- Single character aliases apply everywhere
 			["b"] = ")",
 			["B"] = "}",
 			["r"] = "]",
-			["q"] = { '"', "'", "`" }, -- Table aliases only apply for changes/deletions
+			-- Table aliases only apply for changes/deletions
+			["q"] = { '"', "'", "`" }, -- Any quote character
+			["s"] = { ")", "]", "}", ">", "'", '"', "`" }, -- Any surrounding delimiter
 		},
+	},
+	highlight_motion = { -- Highlight before inserting/changing surrounds
+		duration = 0,
 	},
 })
