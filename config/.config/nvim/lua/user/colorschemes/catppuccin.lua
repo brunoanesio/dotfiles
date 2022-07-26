a6dfd4a329d7523054f86529a888e7ea6be22b75
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2022-07-26 10:06:36 -0300

    Update catppuccin config

--- config/.config/nvim/lua/user/colorschemes/catppuccin.lua
+++ config/.config/nvim/lua/user/colorschemes/catppuccin.lua
@@ -2,6 +2,7 @@ local status_ok, catppuccin = pcall(require, "catppuccin")
 if not status_ok then
 	return
 end
+vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
 
 catppuccin.setup({
 	styles = {
@@ -47,5 +48,4 @@ catppuccin.setup({
 	},
 })
 
-vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
-catppuccin.load()
+vim.cmd([[colorscheme catppuccin]])
