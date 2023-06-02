8b8edb1ae53980120c0e9ef7e15677f8003346ce
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2023-06-02 17:51:27 -0300

    Update wezterm config

--- config/.config/wezterm/wezterm.lua
+++ config/.config/wezterm/wezterm.lua
@@ -1,25 +1,27 @@
 local wezterm = require("wezterm")
 
-return {
-	front_end = "OpenGL",
-	font = wezterm.font("Iosevka Term Nerd Font", { weight = "Medium" }),
-	font_size = 14,
-	line_height = 1.0,
-	underline_position = -4,
-	underline_thickness = 2,
-	cursor_thickness = 1,
-	color_scheme = "Catppuccin Mocha",
-	use_fancy_tab_bar = false,
-	hide_tab_bar_if_only_one_tab = true,
-	tab_bar_at_bottom = true,
-	window_padding = {
-		left = 10,
-		right = 10,
-		top = 10,
-		bottom = 10,
-	},
-	window_background_opacity = 0.85,
-	text_background_opacity = 1.0,
-	enable_wayland = true,
-	warn_about_missing_glyphs = false,
+local config = {
+  front_end = "OpenGL",
+  font = wezterm.font("Iosevka SS07", { weight = "Medium" }),
+  font_size = 14,
+  line_height = 1.0,
+  underline_position = -4,
+  underline_thickness = 2,
+  cursor_thickness = 1,
+  color_scheme = "Catppuccin Mocha",
+  use_fancy_tab_bar = false,
+  hide_tab_bar_if_only_one_tab = true,
+  tab_bar_at_bottom = true,
+  window_padding = {
+    left = 10,
+    right = 10,
+    top = 10,
+    bottom = 10,
+  },
+  window_background_opacity = 0.85,
+  text_background_opacity = 1.0,
+  enable_wayland = true,
+  warn_about_missing_glyphs = false,
 }
+
+return config
