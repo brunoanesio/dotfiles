cc45a967f5b7512391e445c0a4d4cd188bd0f5a0
Bruno Anesio <69819132+brunoanesio@users.noreply.github.com>
2023-09-08 20:30:04 -0300

    updates

--- config/.config/wezterm/wezterm.lua
+++ config/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local config = {
   front_end = "OpenGL",
-  font = wezterm.font("Iosevka SS07", { weight = "Medium" }),
+  font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" }),
   font_size = 14,
   line_height = 1.0,
   underline_position = -4,
@@ -13,12 +13,12 @@ local config = {
   hide_tab_bar_if_only_one_tab = true,
   tab_bar_at_bottom = true,
   window_padding = {
-    left = 10,
-    right = 10,
-    top = 10,
-    bottom = 10,
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
   },
-  window_background_opacity = 0.85,
+  window_background_opacity = 0.90,
   text_background_opacity = 1.0,
   enable_wayland = true,
   warn_about_missing_glyphs = false,
